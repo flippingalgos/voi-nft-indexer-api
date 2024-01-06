da0337673fcba95357ed3465b4ef5c1f3910eb1f
FlippingAlgos <scottbolasevich@gmail.com>
2024-01-06 12:13:22 -0500

    testing another test endpoint w 2 parameters

---
+++ pages/api/v1/sales.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+/**
+ * @swagger
+ * /api/v1/sales:
+ *   get:
+ *     description: Get Sales by asa_id
+ *     parameters:
+ *        - name: asa_id
+ *          in: path
+ *          required: true
+ *        - name: x-access-token
+ *          in: header
+ *          required: true
+ *     responses:
+ *       200:
+ *         description: return object of one events
+ */
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") return res.status(400).json({
+    error: "REQUEST METHOD NOT VALID only GET",
+    success: false
+  }) 
+
+  const {asa_id} = req.query;
+  if(asa_id){
+    res.status(200).json({error: false, message: "asa sales found: " + asa_id});
+  }else{
+    res.status(400).json({error: true, message: "sales for asa_id not found"});
+  }
+};
+
+export default handler;
