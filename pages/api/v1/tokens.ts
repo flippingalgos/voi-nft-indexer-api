1b8201e580266755cd2f37fea9ab3cc5a4f6a3e9
FlippingAlgos <scottbolasevich@gmail.com>
2024-01-06 12:29:48 -0500

    changign all the versions cause i dunno why

--- pages/api/v1/tokens.ts
+++ pages/api/v1/tokens.ts
@@ -8,12 +8,38 @@ import { NextApiRequest, NextApiResponse } from 'next'
  *     produces:
  *       - application/json
  *     parameters:
- *        - name: round
- *          in: query
- *          required: true
- *        - name: owner
- *          in: query
- *          required: true
+ *       - name: round
+ *         in: query
+ *         description: Include results for the specified round.
+ *         type: integer
+ *       - name: next
+ *         in: query
+ *         description: Token for the next page of results. Use the `next-token` provided by the previous page of results.
+ *         type: string
+ *       - name: limit
+ *         in: query
+ *         description: Maximum number of results to return.
+ *         type: integer
+ *       - name: contractId
+ *         in: query
+ *         description: Limit results to NFTs implemented by the given contract ID.
+ *         type: integer
+ *       - name: tokenId
+ *         in: query
+ *         description: Limit results to NFTs with the given token ID.
+ *         type: integer
+ *       - name: owner
+ *         in: query
+ *         description: Limit results to NFTs owned by the given owner.
+ *         type: string
+ *       - name: mint-min-round
+ *         in: query
+ *         description: Limit results to NFTs minted on or after the given round.
+ *         type: integer
+ *       - name: mint-max-round
+ *         in: query
+ *         description: Limit results to NFTs minted on or before the given round.
+ *         type: integer
  *     responses:
  *       '200':
  *         description: Successful response
