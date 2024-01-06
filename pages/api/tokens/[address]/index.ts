749f6d82e4866e773f5814681bbc039bc476410d
rgranata1 <rgranata1@gmail.com>
2024-01-06 09:30:11 -0500

    update example API endpoints to ARC-74 spec

--- pages/api/tokens/[address]/index.ts
+++ pages/api/tokens/[address]/index.ts
@@ -1,21 +1,104 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
+
 /**
  * @swagger
- * /api/tokens/{address}:
+ * /nft-indexer/v1/tokens:
  *   get:
- *     description: Get Tokens By Address
+ *     summary: Retrieve NFT tokens based on query parameters
+ *     produces:
+ *       - application/json
  *     parameters:
- *        - name: address
- *          in: path
- *          required: true
+ *       - name: round
+ *         in: query
+ *         description: Include results for the specified round.
+ *         type: integer
+ *       - name: next
+ *         in: query
+ *         description: Token for the next page of results. Use the `next-token` provided by the previous page of results.
+ *         type: string
+ *       - name: limit
+ *         in: query
+ *         description: Maximum number of results to return.
+ *         type: integer
+ *       - name: contractId
+ *         in: query
+ *         description: Limit results to NFTs implemented by the given contract ID.
+ *         type: integer
+ *       - name: tokenId
+ *         in: query
+ *         description: Limit results to NFTs with the given token ID.
+ *         type: integer
+ *       - name: owner
+ *         in: query
+ *         description: Limit results to NFTs owned by the given owner.
+ *         type: string
+ *       - name: mint-min-round
+ *         in: query
+ *         description: Limit results to NFTs minted on or after the given round.
+ *         type: integer
+ *       - name: mint-max-round
+ *         in: query
+ *         description: Limit results to NFTs minted on or before the given round.
+ *         type: integer
  *     responses:
- *       200:
- *         description: return object
- *       400:
- *         description: return was unsuccessful
- *       500:
- *         description: return was unsuccessful
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TokenResponse'
+ *       '400':
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *
+ * components:
+ *   schemas:
+ *     Token:
+ *       type: object
+ *       properties:
+ *         owner:
+ *           type: string
+ *         contractId:
+ *           type: integer
+ *         tokenId:
+ *           type: integer
+ *         mint-round:
+ *           type: integer
+ *         metadataURI:
+ *           type: string
+ *         metadata:
+ *           type: object
+ *
+ *     TokenResponse:
+ *       type: object
+ *       properties:
+ *         tokens:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Token'
+ *         current-round:
+ *           type: integer
+ *         next-token:
+ *           type: string
+ *
+ *     Error:
+ *       type: object
+ *       properties:
+ *         data:
+ *           type: object
+ *         message:
+ *           type: string
  */
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET") return res.status(400).json({
     error: true,
--- pages/api/transfers/[address]/index.ts
+++ pages/api/transfers/[address]/index.ts
@@ -1,21 +1,109 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+
 /**
  * @swagger
- * /api/transfers/{address}:
+ * /nft-indexer/v1/transfers:
  *   get:
- *     description: Get Transfers by address
+ *     summary: Retrieve NFT transfers based on query parameters
+ *     produces:
+ *       - application/json
  *     parameters:
- *        - name: address
- *          in: path
- *          required: true
+ *       - name: round
+ *         in: query
+ *         description: Include results for the specified round. For performance reasons, this parameter may be disabled on some configurations.
+ *         type: integer
+ *       - name: next
+ *         in: query
+ *         description: Token for the next page of results. Use the `next-token` provided by the previous page of results.
+ *         type: string
+ *       - name: limit
+ *         in: query
+ *         description: Maximum number of results to return. There could be additional pages even if the limit is not reached.
+ *         type: integer
+ *       - name: contractId
+ *         in: query
+ *         description: Limit results to NFTs implemented by the given contract ID.
+ *         type: integer
+ *       - name: tokenId
+ *         in: query
+ *         description: Limit results to NFTs with the given token ID.
+ *         type: integer
+ *       - name: user
+ *         in: query
+ *         description: Limit results to transfers where the user is either the sender or receiver.
+ *         type: string
+ *       - name: from
+ *         in: query
+ *         description: Limit results to transfers with the given address as the sender.
+ *         type: string
+ *       - name: to
+ *         in: query
+ *         description: Limit results to transfers with the given address as the receiver.
+ *         type: string
+ *       - name: min-round
+ *         in: query
+ *         description: Limit results to transfers that were executed on or after the given round.
+ *         type: integer
+ *       - name: max-round
+ *         in: query
+ *         description: Limit results to transfers that were executed on or before the given round.
+ *         type: integer
  *     responses:
- *       200:
- *         description: return object
- *       400:
- *         description: return was unsuccessful
- *       500:
- *         description: return was unsuccessful
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TransferResponse'
+ *       '400':
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ * components:
+ *   schemas:
+ *     Transfer:
+ *       type: object
+ *       properties:
+ *         contractId:
+ *           type: integer
+ *         tokenId:
+ *           type: integer
+ *         from:
+ *           type: string
+ *         to:
+ *           type: string
+ *         round:
+ *           type: integer
+ * 
+ *     TransferResponse:
+ *       type: object
+ *       properties:
+ *         transfers:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Transfer'
+ *         current-round:
+ *           type: integer
+ *         next-token:
+ *           type: string
+ *     
+ *     Error:
+ *       type: object
+ *       properties:
+ *         data:
+ *           type: object
+ *         message:
+ *           type: string
  */
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET") return res.status(400).json({
     error: true,
